1c0afc38268612a62b266daf02d42a6eb560832e
medkalil <medkalilbenahmed@gmail.com>
2024-08-05 10:19:31 +0100

    footer

---
+++ app/(landingpage)/_component/footer.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="max-w-7xl px-5 z-50 mx-auto flex items-center flex-wrap md:justify-between justify-center md:space-y-0 space-y-3">
+      <Image
+        src={"/logo-black.svg"}
+        alt=""
+        width={130}
+        height={130}
+        className="dark:hidden"
+      />
+      <Image
+        src={"/logo-white.svg"}
+        alt=""
+        width={130}
+        height={130}
+        className="hidden dark:block "
+      />
+      <div className="flex items-center flex-wrap gap-2">
+        <Button variant={"link"}>
+            Privacy Policy
+        </Button>
+        <Button variant={"link"}>
+            Terms of Service
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/(landingpage)/layout.tsx
+++ app/(landingpage)/layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./_component/navbar";
+import Footer from "./_component/footer";
 
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Navbar />
       <main className="h-full">{children}</main>
-      <div> footer</div>
+      <Footer />
     </div>
   );
 };
